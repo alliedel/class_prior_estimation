485be7f68650cab4e3b01ddb31ec44db94161900
Matt Barnes <barnes.matt.j@gmail.com>
2015-03-30 11:41:55 -0400

    Method for computing mean squared error

---
+++ computeMSE.m
@@ -0,0 +1,13 @@
+function [ MSE ] = computeMSE( priors1, priors2 )
+%COMPUTEMSE Computes the mean squared error
+%   Inputs:
+%       priors1 - vector of priors, one per class
+%       priors2 - vector of priors, classes in same order as priors1
+%
+%   Outputs:
+%       MSE - the mean squared error
+
+MSE = sum((priors1-priors2).^2);
+
+end
+
